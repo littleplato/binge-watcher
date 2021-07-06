7234b596d9e9638996229a281fd054c8f283bc0f
Wee Jerrick <weejerrick@gmail.com>
2021-07-06 17:16:56 +0800

    integrated mui link with react-router-dom

--- binge-watcher-app/src/App.js
+++ binge-watcher-app/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { Link as RouterLink } from 'react-router-dom';
 import Generate from "./pages/Generate";
 import Watchplan from "./pages/Watchplan";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
+import Contact from "./pages/Contact";
+import { Link } from "@material-ui/core";
+
 
 const theme = createMuiTheme({
   // palette: {
@@ -16,23 +20,23 @@ const theme = createMuiTheme({
   // },
   typography: {
     fontFamily: "Alegreya Sans",
-		fontWeightLight: 100,
-		fontWeightRegular: 400,
-		fontWeightMedium: 500,
-		fontWeightBold: 700,
-  }
+    fontWeightLight: 100,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
 });
 
 const useStyles = makeStyles({
   toolbar: {
     marginTop: 80,
   },
-  logo : {
-    flexGrow: 1
+  logo: {
+    flexGrow: 1,
   },
   link: {
-    marginRight: 20
-  }
+    marginRight: 20,
+  },
 });
 
 function App() {
@@ -43,16 +47,16 @@ function App() {
       <AppBar>
         <Toolbar>
           <Typography variant="h5" className={classes.logo}>
-            The Binge Watcher
+            <Link component={RouterLink} color="inherit" to="/">The Binge Watcher</Link>
+          </Typography>
+          <Typography  className={classes.link}>
+            <Link component={RouterLink} color="inherit" to="/">Generate Plan</Link>
           </Typography>
           <Typography className={classes.link}>
-            Generate Plan
-          </Typography>
-          <Typography className={classes.link}>
-            Watchplan
+            <Link component={RouterLink} color="inherit" to="/watchplan">Watchplan</Link>
           </Typography>
           <Typography>
-            Contact
+            <Link component={RouterLink} color="inherit" to="/contact">Contact</Link>
           </Typography>
         </Toolbar>
       </AppBar>
@@ -64,6 +68,9 @@ function App() {
         </Route>
         <Route exact path="/watchplan">
           <Watchplan />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
         </Route>
         <Redirect to="/" />
       </Switch>
